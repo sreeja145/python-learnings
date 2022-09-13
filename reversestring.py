27a18f5440568e5547da5492d201bc1a2e64a2cf
Nimmagadda Pavana Sreeja <73514771+sreeja145@users.noreply.github.com>
2022-09-13 18:22:45 +0530

    Create reversestring.py

---
+++ reversestring.py
@@ -0,0 +1,15 @@
+""" Write a program to get a string as input and reverse the string without using temporary variable.
+
+Input Description:
+A single line containing a string.
+
+Output Description:
+Print the reversed string.
+
+Sample Input :
+GUVI
+Sample Output :
+IVUG """
+original_string=input()
+txt=original_string[::-1]
+print(txt)
